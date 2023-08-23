a6a2cd760fd876e14b0f1a2ca675b85fcaa00d20
kushan2k <kushangayantha001@gmail.com>
2023-08-23 20:44:27 +0530

    deployed to local hardhat node

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,5 +17,12 @@ module.exports = {
       hardhat: {
          chainId: 1337,
       },
+      localhost: {
+         url: "http://127.0.0.1:8545/",
+         accounts: [
+            "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+            "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+         ],
+      },
    },
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,20 +4,11 @@ async function main() {
    const contract = await hre.ethers.getContractFactory("FundMe")
 
    //deploying contract
-   // console.log("Deploying FundMe...")
-   // const FundMe = await contract.deploy()
-   // const address = await FundMe.getAddress()
-
-   const address = "0x618E91f76c173Fc15b5E2F430c324fF121F94A54"
+   console.log("Deploying FundMe...")
+   const FundMe = await contract.deploy()
+   const address = await FundMe.getAddress()
 
    console.log("FundMe deployed to:", address)
-
-   //attact the contract
-   const FundMe = contract.attach(address)
-
-   const owner = await FundMe.viewOwner()
-
-   console.log(`owner is ${owner}`)
 }
 
 main()
