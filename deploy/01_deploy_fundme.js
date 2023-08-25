f5e64a331da69963c81fbde76c60e1baeffec112
kushan2k <kushangayantha001@gmail.com>
2023-08-25 13:08:02 +0530

    deploy fundme contract with out considering the chain

--- deploy/01_deploy_fundme.js
+++ deploy/01_deploy_fundme.js
@@ -1,19 +1,21 @@
 const { priceFeedAddress, getActiveChain } = require("../helder.hardhat.config")
-const { network } = require("hardhat")
+const { network, deployments } = require("hardhat")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
+   let priceFeed
    if (getActiveChain(network.name)) {
-      console.log("Development chains, skipping....")
-      return
+      console.log("Deployment chain detected!")
+      const MockV3 = await deployments.get("MockV3Aggregator")
+      priceFeed = MockV3.address
+   } else {
+      //get the price feed addres depending on the chain id
+      priceFeed = priceFeedAddress[network.config.chainId]["address"]
    }
 
    const { deploy } = deployments
    const { deployer } = await getNamedAccounts()
 
-   //get the price feed addres depending on the chain id
-   const priceFeed = priceFeedAddress[network.config.chainId]["address"]
-
-   const FundMe = await deploy("FundMe", {
+   await deploy("FundMe", {
       from: deployer,
       args: [priceFeed],
       log: true,
